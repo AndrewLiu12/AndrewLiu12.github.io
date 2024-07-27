62338925b0471c3b7a762ce21ca5df46f44e4c3f
Andrew Liu <liua@bu.edu>
2024-07-27 12:31:18 -0400

    add js

--- my_script.js
+++ my_script.js
@@ -1,1 +1,11 @@
 window.slide = new SlideNav();
+
+var header = document.getElementById("header");
+var navs = header.getElementsByClassName("nav");
+for (var i = 0; i < navs.length; i++) {
+    navs[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
